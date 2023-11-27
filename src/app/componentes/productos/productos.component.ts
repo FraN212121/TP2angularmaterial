0d19ce83b809704c8e4dfe205824b5d06a89734d
FraN212121 <146133215+FraN212121@users.noreply.github.com>
2023-11-27 17:16:49 -0300

    Add files via upload

--- src/app/componentes/productos/productos.component.ts
+++ src/app/componentes/productos/productos.component.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent {
-  filtros = [
-    
+  mostrar1 = false;
+  consolas = [
+    { nombre: "PlayStation 5", precio: "US$550", img: '../../../assets/imagenes/ps5 fisica.jpg' },
+    { nombre: "PlayStation 5 Edicion Digital", precio: "US$450", img: '../../../assets/imagenes/ps5 digital edition.jpg' },
+    { nombre: "PlayStation 5 Spider-Man Edition", precio: "US$700", img: '../../../assets/imagenes/ps5 spiderman edition.jpg' },
+    { nombre: "PlayStation 4", precio: "US$300", img: '../../../assets/imagenes/ps4 nomral.webp' },
+    { nombre: "PlayStation 4 Slim", precio: "US$275", img: '../../../assets/imagenes/ps4 slim.webp' },
+    { nombre: "PlayStation 4 Pro", precio: "US$325", img: '../../../assets/imagenes/ps4 pro.webp' }
   ]
+
 }
+
+
+/* consolas[
+  { nombre: "PlayStation 5", precio: "US$550", img: '../../../assets/imagenes/ps5 fisica.jpg' },
+  { nombre: "PlayStation 5 Edicion Digital", precio: "US$450", img: '../../../assets/imagenes/ps5 digital edition.jpg' },
+  { nombre: "PlayStation 5 Spider-Man Edition", precio: "US$700", img: '../../../assets/imagenes/ps5 spiderman edition.jpg' },
+  { nombre: "PlayStation 4", precio: "US$300", img: '../../../assets/imagenes/ps4 nomral.webp' },
+  { nombre: "PlayStation 4 Slim", precio: "US$275", img: '../../../assets/imagenes/ps4 slim.webp' },
+  { nombre: "PlayStation 4 Pro", precio: "US$325", img: '../../../assets/imagenes/ps4 pro.webp' },
+] */
