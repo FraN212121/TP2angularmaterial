204d1bd9a706cdda4e8367a3b69be5b44f7dc126
FraN212121 <146133215+FraN212121@users.noreply.github.com>
2023-11-28 17:09:45 -0300

    Add files via upload

--- src/app/componentes/productos/productos.component.ts
+++ src/app/componentes/productos/productos.component.ts
@@ -8,14 +8,34 @@ import { Component } from '@angular/core';
 export class ProductosComponent {
   mostrar1 = false;
   consolas = [
-    { nombre: "PlayStation 5", precio: "US$550", img: '../../../assets/imagenes/ps5 fisica.jpg' },
-    { nombre: "PlayStation 5 Edicion Digital", precio: "US$450", img: '../../../assets/imagenes/ps5 digital edition.jpg' },
-    { nombre: "PlayStation 5 Spider-Man Edition", precio: "US$700", img: '../../../assets/imagenes/ps5 spiderman edition.jpg' },
-    { nombre: "PlayStation 4", precio: "US$300", img: '../../../assets/imagenes/ps4 nomral.webp' },
-    { nombre: "PlayStation 4 Slim", precio: "US$275", img: '../../../assets/imagenes/ps4 slim.webp' },
-    { nombre: "PlayStation 4 Pro", precio: "US$325", img: '../../../assets/imagenes/ps4 pro.webp' }
+    { nombre: "PlayStation 5", precio: "US$549.99", img: '../../../assets/imagenes/ps5 fisica.jpg' },
+    { nombre: "PlayStation 5 Edicion Digital", precio: "US$449.99", img: '../../../assets/imagenes/ps5 digital edition.jpg' },
+    { nombre: "PlayStation 5 Spider-Man Edition", precio: "US$699.99", img: '../../../assets/imagenes/ps5 spiderman edition.jpg' },
+    { nombre: "PlayStation 4", precio: "US$299.99", img: '../../../assets/imagenes/ps4 nomral.webp' },
+    { nombre: "PlayStation 4 Slim", precio: "US$274.99", img: '../../../assets/imagenes/ps4 slim.webp' },
+    { nombre: "PlayStation 4 Pro", precio: "US$324.99", img: '../../../assets/imagenes/ps4 pro NUEVA.jpg' }
   ]
 
+  mostrar2 = false;
+  accesorios = [
+    { nombre: "Control inalámbrico DualSense®", precio: "US$69.99", img: '../../../assets/imagenes/mando ps5.webp' },
+    { nombre: "Control inalámbrico DualSense Edge™", precio: "US$199.99", img: '../../../assets/imagenes/mando chetado ps5.webp' },
+    { nombre: "Control inalámbrico DUALSHOCK 4", precio: "US$59.99", img: '../../../assets/imagenes/mando ps4.webp' },
+    {
+      nombre: "Auriculares inalámbricos PULSE 3D™", precio: "US$99.99", img: '../../../assets/imagenes/auriculares ps5.webp'
+    },
+    { nombre: "Cámara HD de PS5", precio: "US$69.99", img: '../../../assets/imagenes/camara ps5.webp' },
+    { nombre: "PlayStation VR2", precio: "US$499.99", img: '../../../assets/imagenes/ps cr2.jpg' }
+  ]
+  mostrar3 = false;
+  juegos = [
+    { nombre: "DEATH STRANDING DIRECTOR'S CUT", precio: "US$59.99", plataforma: "PS5", img: '../../../assets/imagenes/death stranding.webp' },
+    { nombre: "Gran Turismo 7", precio: "US$69.99", plataforma: "PS4, PS5", img: "../../../assets/imagenes/gran turismo 7.webp" },
+    { nombre: "Ghost of Tsushima Director's Cut", precio: "US$69.99", plataforma: "PS4, PS5", img: "../../../assets/imagenes/ghost of tsushima.jpg" },
+    { nombre: "Horizon Forbidden West", precio: "US$49.99", plataforma: "PS4, PS5", img: "../../../assets/imagenes/horizon forbidden westr.webp" },
+    { nombre: "Marvel's Spider-Man: Miles Morales", precio: "US$49.99", plataforma: "PS4 PS5", img: "../../../assets/imagenes/spiderman miles morales.webp" },
+    { nombre: "The Last of Us™ Part I", precio: "US$69.99", plataforma: "PS5", img: "../../../assets/imagenes/the last of us part 1.webp" },
+  ]
 }
 
 
--- src/app/componentes/servicios/servicios.component.ts
+++ src/app/componentes/servicios/servicios.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+export interface Consolas {
+  nombre: string;
+  numero: number;
+  precio: number;
+  plataforma: string;
+}
+
+
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.css']
 })
-export class ServiciosComponent {
 
+
+
+
+
+
+export class ServiciosComponent {
+  displayedColumns: string[] = ['numero', 'nombre', 'precio', 'plataforma'];
+  consolasTabla = consolas;
 }
+
+const consolas: Consolas[] = [
+  {numero: 1, nombre: 'PlayStation 5', precio: 549.99, plataforma: 'PS5'},
+  {numero: 3, nombre: 'PlayStation 5 Edicion Digital', precio: 449.99, plataforma: 'PS5'},
+  {numero: 2, nombre: 'PlayStation 5 Spider-Man Edition', precio: 699.99, plataforma: 'PS5'},
+  {numero: 4, nombre: 'PlayStation 4', precio: 299.99, plataforma: 'PS4'},
+  {numero: 5, nombre: 'PlayStation 4 Slim', precio: 274.99, plataforma: 'PS4'},
+  {numero: 6, nombre: 'PlayStation 4 Pro', precio: 349.99, plataforma: 'PS4'},
+  {numero: 7, nombre: 'PlayStation 4 Pro Death Stranding Edition', precio: 449.99, plataforma: 'PS4'},
+  {numero: 8, nombre: 'PlayStation 4 Pro The Last Of Us Part 2 Edition', precio: 449.99, plataforma: 'PS4'},
+  {numero: 9, nombre: 'PlayStation 4 Pro Blue Edition', precio: 449.99, plataforma: 'PS4'},
+];
+
